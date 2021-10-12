063edb494a9b34cf086da17a3b0c9fc9f856241e
Raston Gilbert <rastong@gmail.com>
2021-10-11 20:43:09 -0400

    first commit

---
+++ Circles/Calculate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Circles
+{
+    class Circle
+    {
+        private double Radius;
+        private double pi;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateCircumference()
+        {
+            double Circ = 2 * Radius * Math.PI;
+            return Circ;
+        }
+
+        private string FormatNumber(double value)
+        {
+            string Format = string.Format("{0:0.00}", value);
+            return Format;
+        }
+
+        public string CalcFormCirc()
+        {
+            string circForm = FormatNumber(CalculateCircumference());
+            return circForm;
+        }
+
+        public double CalculateArea()
+        {
+            double area = (Radius * Radius) * Math.PI;
+            return area;
+        }
+
+        public string CalcFormArea()
+        {
+            string circForm = FormatNumber(CalculateArea());
+            return circForm;
+        }
+    }
+}
---
+++ Circles/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Circles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            bool runProgram = true;
+            int CircleNum = 0;
+            while (runProgram)
+            {
+                
+                while (true)
+                {
+
+                    
+                    try
+                    {
+                        Console.WriteLine("Hello Please enter the radius of your circle");
+                        string input = Console.ReadLine();
+                        bool legitNum = double.TryParse(input, out radius);
+                        if (legitNum && radius > 0)
+                        {
+                            CircleNum += 1;
+
+                            break;
+                        }
+                        else if (legitNum)
+                        {
+                            throw new Exception("Please try a positive number");
+                        }
+                        else
+                        {
+                            throw new Exception("Not a vaild in put, please try again with an integar");
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Not a valid input. Try again");
+                    }
+                }
+                Circle MyCircle = new Circle(radius);
+                string circ = MyCircle.CalcFormCirc();
+                Console.WriteLine($"Circumference: {circ}");
+                string circArea = MyCircle.CalcFormArea();
+                Console.WriteLine($"Area: {circArea}");
+                while (true)
+                {
+                    Console.WriteLine("Continue? y/n");
+                    string input = Console.ReadLine();
+
+                    if (input.ToLower() == "y")
+                    {
+                        break;
+                    }
+                    else if (input.ToLower() == "n")
+                    {
+                        runProgram = false;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter y or n");
+                    }
+                }
+               
+            }
+            Console.WriteLine($"Goodbye. You created {CircleNum} Circle objects.");
+        }
+    }
+}
